bda0e44a4d19be2e6609f4a18b69ac2c2ff9f22a
Sylwia Pytko <sylwia.pytko@gmail.com>
2017-12-25 16:51:46 +0100

    backend: correction to /api/trip

--- api/src/main/java/com/projekt/ai/api/controller/TripController.java
+++ api/src/main/java/com/projekt/ai/api/controller/TripController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value="/api/book")
+@RequestMapping(value="/api/trip")
 public class TripController {
 
     @Autowired
