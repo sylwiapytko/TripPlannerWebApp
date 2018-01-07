5456758890413237214238ea8847293d4b495a57
Sylwia Pytko <sylwia.pytko@gmail.com>
2018-01-07 20:41:05 +0100

    frotntend: dateformat dd.mm.yyyy

--- frontend/app/tripDestinations/tripDestinationDetails/tripDestinationDetails.js
+++ frontend/app/tripDestinations/tripDestinationDetails/tripDestinationDetails.js
@@ -8,13 +8,21 @@ angular.module('myApp.tripDestinationDetails', ['ngRoute'])
             controller: 'tripDestinationDetailsCtrl'
         };
     })
+    .config(['$mdDateLocaleProvider', function($mdDateLocaleProvider) { $mdDateLocaleProvider.formatDate = function(date) {
+        if (moment(date).isValid()) {
+            return moment(date).format("DD.MM.YYYY");
+        }
+        return null;
+    };
+    }])
     .controller('tripDestinationDetailsCtrl', function($scope, $http, $routeParams) {
 
         $scope.test=$routeParams.id;
         var url = "http://localhost:8080/api/trip/getDestination/" + $routeParams.id;
         $http.get(url).then(function(response) {
             $scope.destination = response.data;
-            // $scope.destination.date_from =new Date($scope.destination.date_from);
+            $scope.dateFrom =new Date($scope.destination.date_from);
+            $scope.dateTo =new Date($scope.destination.date_to);
         });
         $scope.editDestination = function() {
             var req = {
