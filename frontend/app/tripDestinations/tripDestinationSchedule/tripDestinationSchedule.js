428875c8e0b65b2cfc715543fe57cf99c8ca1f12
Sylwia Pytko <sylwia.pytko@gmail.com>
2018-01-11 04:06:47 +0100

    frontend: destinationScheduler startDate

--- frontend/app/tripDestinations/tripDestinationSchedule/tripDestinationSchedule.js
+++ frontend/app/tripDestinations/tripDestinationSchedule/tripDestinationSchedule.js
@@ -9,20 +9,70 @@ angular.module('myApp.tripDestinationSchedule', ['ngRoute'])
         };
     })
     .controller('tripDestinationScheduleCtrl', function($scope, $http, $routeParams, $rootScope) {
-        var getDestinationLodging= function(){
-            $scope.test=$routeParams.id;
-            var url = "http://localhost:8080/api/trip/getDestinationLodging/" + $routeParams.id;
-            $http.get(url).then(function(response) {
-                $scope.lodgings = response.data;
-            });
-        }
 
-        $scope.editLodging = function () {
-            $rootScope.$broadcast('editLodging');
-        }
+    var url = "http://localhost:8080/api/trip/getDestination/" + $routeParams.id;
+    $http.get(url).then(function(response) {
+        $scope.destination = response.data;
+        $scope.dateFrom =new Date($scope.destination.date_from);
+        configStartDate();
+    });
+    var configStartDate = function () {
+        $scope.dateConfigstart= $scope.dateFrom.format("yyyy-mm-dd");
+        $scope.weekConfig = {startDate: $scope.dateConfigstart};
+        $scope.monthConfig = {startDate: $scope.dateConfigstart}
+    };
 
-        $rootScope.$on('lodgingUpdated', function () {
-            getDestinationLodging();
-        });
-        getDestinationLodging();
+        $scope.showMonth = function() {
+            $scope.monthConfigVisible = true;
+            $scope.weekConfig.visible = false;
+        };
+        $scope.showWeek = function() {
+            $scope.monthConfigVisible = false;
+            $scope.weekConfig.visible = true;
+        };
+        $scope.weekConfig = {
+            viewType: "Week",
+            startDate: new DayPilot.Date(),
+
+            onEventMove: function(args) {
+                var params = {
+                    id: args.e.id(),
+                    newStart: args.newStart.toString(),
+                    newEnd: args.newEnd.toString()
+                }
+                //http.post
+            },
+            onEventClick: function(args) {
+                var modal = new DayPilot.Modal({
+                    onClosed: function(args) {
+                        if (args.result) {  // args.result is empty when modal is closed without submitting
+                            loadEvents();
+                        }
+                    }
+                });
+                //show dialog and ask for name and other info
+            }
+        };
+        $scope.monthConfig = {
+            startDate: new DayPilot.Date(),
+
+            onEventMove: function(args) {
+                var params = {
+                    id: args.e.id(),
+                    newStart: args.newStart.toString(),
+                    newEnd: args.newEnd.toString()
+                }
+                //http.post
+            },
+            onEventClick: function(args) {
+                var modal = new DayPilot.Modal({
+                    onClosed: function(args) {
+                        if (args.result) {  // args.result is empty when modal is closed without submitting
+                            loadEvents();
+                        }
+                    }
+                });
+                //show dialog and ask for name and other info
+            }
+        };
     });
